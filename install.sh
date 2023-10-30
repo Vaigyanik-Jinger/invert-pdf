22e0402e018dd6848b64f3fae461b534b5b4b6b1
Harsh Jinger <60634573+Vaigyanik-Jinger@users.noreply.github.com>
2023-10-30 16:17:40 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y imagemagick ocrmypdf ghostscript
+mkdir ~/Scripts
+cd ~/Scripts
+mkdir invert-pdf
+cd invert-pdf
+curl -L -O ""
+cd 
+new_alias="alias invertpdf='bash ~/Scripts/invert-pdf/invert-pdf.sh'"
+# Check if the alias is already in .bashrc to avoid duplicates
+if ! grep -qF "$new_alias" ~/.bashrc; then
+    # Append the alias to .bashrc
+    echo "$new_alias" >> ~/.bashrc
+    echo "Alias added to .bashrc"
+else
+    echo "Alias already exists in .bashrc, no changes made."
+fi
+cd
